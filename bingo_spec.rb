5e6a3a353a193c0332f8c000fe7aeada7357b972
Kento Sato <tatehito.st@gmail.com>
2019-05-21 20:49:25 +0900

    initialize

--- bingo_spec.rb
+++ bingo_spec.rb
@@ -2,19 +2,74 @@
 
 class Bingo
   def self.generate_card
-    # 以下の行は削除して、自分でロジックを実装してください。
-    sample
+    header = " B |  I |  N |  G |  O"
+
+    # 各列の数字を配列で取得する
+    @columnB = generate_numbers_column("B")
+    @columnI = generate_numbers_column("I")
+    @columnN = generate_numbers_column("N")
+    @columnG = generate_numbers_column("G")
+    @columnO = generate_numbers_column("O")
+
+    # 各行の文字列を作成する
+    line0 = generate_line_string(0)
+    line1 = generate_line_string(1)
+    line2 = generate_line_string(2)
+    line3 = generate_line_string(3)
+    line4 = generate_line_string(4)
+
+    # ビンゴカードを出力する
+    <<-card
+#{header}
+#{line0}
+#{line1}
+#{line2}
+#{line3}
+#{line4}
+    card
   end
 
-  def self.sample
-    <<-CARD
- B |  I |  N |  G |  O
-13 | 22 | 32 | 48 | 61
- 3 | 23 | 43 | 53 | 63
- 4 | 19 |    | 60 | 65
-12 | 16 | 44 | 50 | 75
- 2 | 28 | 33 | 56 | 68
-    CARD
+  # 1列分の数字を生成し配列を返す
+  def self.generate_numbers_column(column)
+    # 生成対象のカラムごとに候補の数字の範囲を定義する
+    if column == "B"
+        numbers = ("1".."15").to_a
+    elsif column == "I"
+        numbers = ("16".."30").to_a
+    elsif column == "N"
+        numbers = ("31".."45").to_a
+    elsif column == "G"
+        numbers = ("46".."60").to_a
+    elsif column == "O"
+        numbers = ("61".."75").to_a
+    end
+    
+    # 候補の数字から5つの数字をランダムに選び配列で返す
+    # 一度選ばれた数字を候補から除くことで同じ数字が選ばれないようにしている
+    column_numbers = []
+    5.times do
+        random = rand(0..numbers.length-1)
+        selected_number = numbers[random]
+        
+        # 一桁の数字が選ばれた場合はスペースを加えて右詰にする
+        if selected_number.length == 1
+            selected_number = " " + selected_number
+        end
+        
+        column_numbers.push(selected_number)
+        numbers.delete_at(random)
+    end
+    column_numbers
+  end
+
+  # 1行分の文字列を生成する
+  def self.generate_line_string(line_number)
+    # 3行目の3列目（中央）は空白をセットする
+    if line_number == 2
+        line = @columnB[line_number] + " | " + @columnI[line_number] + " | " + "  "                  + " | " + @columnG[line_number] + " | " + @columnO[line_number]
+    else
+        line = @columnB[line_number] + " | " + @columnI[line_number] + " | " + @columnN[line_number] + " | " + @columnG[line_number] + " | " + @columnO[line_number] 
+    end
   end
 end
 
@@ -78,7 +133,6 @@ describe Bingo do
       expect(numbers_by_col.flatten.uniq).to eq numbers_by_col.flatten
     end
     it '毎回結果が変わること' do
-      pending '独自のコードを実装したらこの行は削除してください'
       cards = 10.times.map { Bingo.generate_card }
       expect(cards.uniq).to eq cards
     end
